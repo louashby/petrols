2a33152522465a1b3265da82aa29f4f2b1552526
qxb0027 <kathrin.zorn@partner.bmw.de>
2013-06-23 22:17:31 +0200

    blog style

--- Site/resources/js/entryItem.js
+++ Site/resources/js/entryItem.js
@@ -12,12 +12,11 @@ function EntryItem(id, image, title, time, shortDescription, link, styleClass) {
 		var blogTile = jQuery('div#' + id);
 		blogTile.append('<div id="imageTile"><img src="' + image + '" /></div>');
 		blogTile.append('<div id="descriptionTile"><div id="descriptionTitle"></div><div id="shortDescription">' + shortDescription + '</div></div>');
+		jQuery(blogTile).find('div#descriptionTitle').append('<h3>' + title + '</h3><h4>' + time + '</h4>');
 		
 		if(link.length > 0){
-			jQuery(blogTile).find('div#descriptionTitle').append('<a href="' + link + '"><h3>' + title + '</h3></a><h4>' + time + '</h4>');
 			jQuery(blogTile).find('div#descriptionTile').append('<a href="' + link + '" style="font-size: 14px; font-weight: bold; float: right; text-decoration: none;">Read more...</a>');
 		} else {
-			jQuery(blogTile).find('div#descriptionTitle').append('<h3>' + title + '</h3><h4>' + time + '</h4>');
 			jQuery(blogTile).find('div#shortDescription').addClass('missingLink');
 		}
 	}
